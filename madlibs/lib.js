a2581a17f824c26fcad08adddf714c3c119cfd99
Ronald Rodriguez <r.rodriguez9696@gmail.com>
2017-09-18 14:49:16 -0400

    Add files via upload

--- madlibs/lib.js
+++ madlibs/lib.js
@@ -1,12 +1,20 @@
+var submit = document.getElementById("submit");
 submit.onclick = function() {
 
-var submit = document.getElementById("lip");
 var name = document.getElementById("name");
 var borough = document.getElementById("borough");
 var noun = document.getElementById("noun");
 var adjective = document.getElementById("adjective");
-var verb= document.getElementById("verb");
-var message= document.getElementById("message");
-message.innerText = "There once was a thing called " + name.value + " from " + borough.value + ". Where the sun always shined and grass was always green. The happy people from " + name.value + " never knew such things as cloudy rainy days or unhappy villagers for that matter. Everyone was happy all the time. Enter and get a glimpse of their reality! Where " + noun.value + " is as good as " + adjective.value + " trying to " + verb.value + " oh boy ain't that some fun. ";
+var verb = document.getElementById("verb");
+// var message = document.getElementById("message");
+
+
+ if (name.value) {
+    message.innerText = "There once was a thing called " + name.value + " from " + borough.value + ". Where the sun always shined and the animals where as small as a " + noun.value + ", and the city skyscrapers smelled as" + adjective.value + " as your middle school bathroom. And the people all enjoy to " + verb.value + " from the very top of the skyscrapers.";
+    
+} else {
+    message.innerText = "Please input a name.";
+}
+
 
 };
