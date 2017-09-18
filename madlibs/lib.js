e86ccb1a284393420571314dd4b5afaee2d9f1fe
Ronald Rodriguez <r.rodriguez9696@gmail.com>
2017-09-18 14:59:52 -0400

    Add files via upload

--- madlibs/lib.js
+++ madlibs/lib.js
@@ -10,10 +10,14 @@ var verb = document.getElementById("verb");
 
 
  if (name.value) {
-    message.innerText = "There once was a thing called " + name.value + " from " + borough.value + ". Where the sun always shined and the animals where as small as a " + noun.value + ", and the city skyscrapers smelled as" + adjective.value + " as your middle school bathroom. And the people all enjoy to " + verb.value + " from the very top of the skyscrapers.";
+    message.innerText = "There once was a thing called " + name.value + " from " + borough.value + ". Where the sun always shined and the animals where as small as a " + noun.value + ", and the city skyscrapers smelled like" + adjective.value + " as your middle school bathroom. And the people all enjoy to " + verb.value + " from the very top of the skyscrapers.";
     
 } else {
     message.innerText = "Please input a name.";
+}
+
+if () {
+    //code
 }
 
 
