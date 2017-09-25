ddc2d06338cc3d43328c001324f55b31157a4271
Ronald Rodriguez <r.rodriguez9696@gmail.com>
2017-09-25 13:08:05 -0400

    Add files via upload

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,61 +1,117 @@
-//var questions = 3;
-// var questionsLeft = ' [' + questions + ' questions left]';
-
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-function buildQuiz(){}
-
-function showResults(){}
-
-// display quiz right away
-buildQuiz();
-
-// on submit, show results
-submitButton.addEventListener('click', showResults);
-
-
-const myQuestions = [
-{                     
-    question: "Who is the strongest?",
-    answers: {
-        a:"Superman",
-        b:"Batman",
-        c:"Thor"
-},      
-  correctAnswer:"c"  
-},
-{
-    question: "What is the name of Thor's hammer?"
-    answers: {
-        a:"Mjölnir",    
-        b:"Ragnarok",
-        c:"adamantium-vibranium"
-    },
-    correctAnswer:"a"
-}, 
-{
-    question:"what is Supermans one weackness?",
-    answers: {
-    a:"Batman",
-    b:"Doomsday",
-    c:"puppies",
-    d:"kryptonite"
-},
-  correctAnswer:"d"  
-}
+var myQuestions = [
+	{
+		question: "Who is the strongest among the three?",
+		answers: {
+			a: 'Superman',
+			b: 'Batman',
+			c: 'Thor'
+		},
+		correctAnswer: 'a'
+	},
+	{
+		question: "What is the name of Thor's hammer?",
+		answers: {
+			a: 'Mjolnir',
+			b: 'Ragnarok',
+			c: 'adamantium-vibranium'
+		},
+		correctAnswer: 'a'
+	},
+    {
+		question: "What is the name of Thor's hammer?",
+		answers: {
+			a: 'Batman',
+			b: 'Doomsday',
+			c: 'Puppies',
+            d: 'kryptonite'
+		},
+		correctAnswer: 'd'
+	}
 ];
 
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 
-//questions -= 1;
-//questionsLeft = ' [' + questions + ' questions left]';
-//var verb = prompt('Please type a verb that does not end in -ing ' + questionsLeft);
-//questions -= 1;
-//questionsLeft = ' [' + questions + ' questions left]';
-//var noun = prompt('Please type a noun' + questionsLeft);
-//alert('All done. Ready for the message?');
-//var sentence = "<h2>There once was a " + adjective;
-//sentence += ' programmer who wanted to use JavaScript to ' + verb;
-//sentence += ' the ' + noun + '.</h2>';
-//document.write(sentence);
+
+	function showQuestions(questions, quizContainer){
+		// we'll need a place to store the output and the answer choices
+		var output = [];
+		var answers;
+
+		// for each question...
+		for(var i=0; i<questions.length; i++){
+			
+			// first reset the list of answers
+			answers = [];
+
+			// for each available answer...
+			for(letter in questions[i].answers){
+
+				// ...add an html radio button
+				answers.push(
+					'<label>'
+						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+						+ letter + ': '
+						+ questions[i].answers[letter]
+					+ '</label>'
+				);
+			}
+
+			// add this question and its answers to the output
+			output.push(
+				'<div class="question">' + questions[i].question + '</div>'
+				+ '<div class="answers">' + answers.join('') + '</div>'
+			);
+		}
+
+		// finally combine our output list into one string of html and put it on the page
+		quizContainer.innerHTML = output.join('');
+	}
+
+
+	function showResults(questions, quizContainer, resultsContainer){
+		
+		// gather answer containers from our quiz
+		var answerContainers = quizContainer.querySelectorAll('.answers');
+		
+		// keep track of user's answers
+		var userAnswer = '';
+		var numCorrect = 0;
+		
+		// for each question...
+		for(var i=0; i<questions.length; i++){
+
+			// find selected answer
+			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+			
+			// if answer is correct
+			if(userAnswer===questions[i].correctAnswer){
+				// add to the number of correct answers
+				numCorrect++;
+				
+				// color the answers green
+				answerContainers[i].style.color = 'lightgreen';
+			}
+			// if answer is wrong or blank
+			else{
+				// color the answers red
+				answerContainers[i].style.color = 'red';
+			}
+		}
+
+		// show number of correct answers out of total
+		resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+	}
+
+	// show questions right away
+	showQuestions(myQuestions, quizContainer);
+	
+	// on submit, show results
+	submitButton.onclick = function(){
+		showResults(myQuestions, quizContainer, resultsContainer);
+	}
+
+
+
+//generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
