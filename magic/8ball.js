661c66af58ae16de21045802f3378afa9402e0d2
Ronald Rodriguez <r.rodriguez9696@gmail.com>
2017-10-02 14:03:43 -0400

    Add files via upload

--- magic/8ball.js
+++ magic/8ball.js
@@ -1,22 +1,16 @@
-var words = [
-             'No',
-             'Defintely not',
-             'As I see it, no',
-             'Most likely .. Not',
-             'Signs points to no',
-             'Ask again later',
-             'Better not tell you',
-             'My repy is no',
-             'My sources say no',
-             'concentrate and ask again'
-             ];
-
-var getRandomWord = function() {
-    return words[math.floor(Math.Random() * words.length)];
+var answers = [
+        'No',
+        'Defintely not',
+        'As I see it, no',
+        'Most likely .. Not',
+        'Signs points to no',
+        'Ask again later',
+        'Better not tell you',
+        'My repy is no',
+        'My sources say no',
+        'concentrate and ask again',
+];
+document.getElementById('answerButton').onclick = function () {
+var answer = answers[Math.floor(Math.random() * answers.length)];
+    document.getElementById('answerContainer').innerHTML = answer;
 };
-
-(function() {('.eball').mouseenter(function(){
-    ('.textbox').html(getRandomWord());
-    });
-
-});
