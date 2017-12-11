2ab3f13253bfcd84cdf4a353ef1fe082a7581266
Ronald Rodriguez <r.rodriguez9696@gmail.com>
2017-12-11 16:39:44 -0500

    Add files via upload

--- pingpong/app.js
+++ pingpong/app.js
@@ -5,25 +5,27 @@ var myScore;
 var myScore1;
 var point1 = 0;
 var point2 = 0;
+var gif1 = document.getElementById("gif1");
+var gif2 = document.getElementById("gif2");
+var gif3 = document.getElementById("gif3");
+
 
 function startGame() {
-myGameArea.start();
+    myGameArea.start();
     
-pong1 = new component(8 , 60 , "yellow" , 20 , 150);
-pong2 = new component(8 , 60 , "lime" , 670 , 150);
-ball = new component(7 , 7 , "orange" , 350, 170);
-myScore = new component("16px" , "consolas" , "yellow" , 200 , 25 , "text");
-myScore1 = new component("16px" , "consolas" , "lime" , 410 , 25 , "text");
-
+    pong1 = new component(8 , 60 , "yellow" , 20 , 150);
+    pong2 = new component(8 , 60 , "lime" , 670 , 150);
+    ball = new component(7 , 7 , "orange" , 350, 170);
+    myScore = new component("16px" , "consolas" , "yellow" , 200 , 25 , "text");
+    myScore1 = new component("16px" , "consolas" , "lime" , 410 , 25 , "text");
 }
 
 var myGameArea = {
-    canvas : document.createElement('canvas'),
+    canvas : document.getElementById('canvas'),
     start : function() {
         this.canvas.width = 700;
         this.canvas.height = 390;
         this.context = this.canvas.getContext('2d');
-        document.body.insertBefore(this.canvas , document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea , 30);
         
         window.addEventListener('keydown' , function(e) {
@@ -55,6 +57,7 @@ function component(width , height , color , x , y , type ) {
     this.y = y;
     this.speedX = 0;
     this.speedY = 0;
+    this.img;
     
     this.update = function() {
         ctx = myGameArea.context;
@@ -62,6 +65,10 @@ function component(width , height , color , x , y , type ) {
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = color;
             ctx.fillText(this.text, this.x, this.y);
+            // getGif(this);
+            if (this.img) {
+               // ctx.drawImage(this.img, this.x, this.y);
+            }
         } else {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -88,7 +95,22 @@ function component(width , height , color , x , y , type ) {
            }
            return crash;
     }
+
+   
 }
+
+function addGif(elem, query) {
+    console.log(elem, query);
+        var key = "uTtk10NFnmdtYP4f3URNps6BFeFqh1Jq";
+    
+        
+        var url = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + key + "&limit=50";
+        $.getJSON(url, function(json) {
+            console.log(json.data);
+            var img = json.data[ Math.floor(Math.random() * json.data.length)];
+            elem.src =  img.images.downsized.url;
+        });
+    }
 
 // Update Game Function //
 
@@ -110,41 +132,41 @@ function updateGameArea(){
     }
     
             
-// Keyboard contract //
-            
-if (myGameArea.keys && myGameArea.keys [38]) {
-    console.log('arrow up');
-    pong1.y -= 10;
-    if(ball.crashWith(pong1)) {
-       ball.speedY = -4;
-       ball.speedX = 14;
+    // Keyboard contract //
+
+    if (myGameArea.keys && myGameArea.keys [38]) {
+        console.log('arrow up');
+        pong1.y -= 10;
+        if(ball.crashWith(pong1)) {
+           ball.speedY = -4;
+           ball.speedX = 14;
+        }
     }
-}
-            
-if (myGameArea.keys && myGameArea.keys [40]) {
-    console.log('arrow down');
-    pong1.y += 10;
-    if(ball.crashWith(pong1)) {
-       ball.speedY = 4;
-       ball.speedX = 14;
+
+    if (myGameArea.keys && myGameArea.keys [40]) {
+        console.log('arrow down');
+        pong1.y += 10;
+        if(ball.crashWith(pong1)) {
+           ball.speedY = 4;
+           ball.speedX = 14;
+        }
     }
-}
-            
-if (myGameArea.keys && myGameArea.keys [37]) {
-    pong2.y -= 10;
-    if(ball.crashWith(pong2)) {
-       ball.speedY = -4;
-       ball.speedX = -8;
-    }
-}   
-            
-if (myGameArea.keys && myGameArea.keys [39]) {
-    pong2.y += 10;
-    if(ball.crashWith(pong2)) {
-       ball.speedY = 4;
-       ball.speedX = -8;
-    }
-}     
+
+    if (myGameArea.keys && myGameArea.keys [37]) {
+        pong2.y -= 10;
+        if(ball.crashWith(pong2)) {
+           ball.speedY = -4;
+           ball.speedX = -8;
+        }
+    }   
+
+    if (myGameArea.keys && myGameArea.keys [39]) {
+        pong2.y += 10;
+        if(ball.crashWith(pong2)) {
+           ball.speedY = 4;
+           ball.speedX = -8;
+        }
+    }     
     
     // Ball Movements //
     
@@ -170,11 +192,12 @@ if (myGameArea.keys && myGameArea.keys [39]) {
     if(ball.x <= 2) {
         ball.x = 690;
         point2 += 1;
-        
+        addGif(gif1, "amazing");
     }
     if(ball.x >= 700) {
         ball.x = 0;
         point1 += 1;
+        addGif(gif2, "happy")    
     }
     
     
@@ -183,7 +206,23 @@ if (myGameArea.keys && myGameArea.keys [39]) {
     pong2.update();
     ball.update();
     
-    myScore.text = "Score : " + point1;
+    if(point1 == 10 || point2 == 10){
+        addGif(gif3,"congrats");
+        
+        point1 = 0;
+        point2 = 0;
+        myGameArea.stop();
+        var restart=document.createElement("button");
+        restart.textContent = "start again";
+        document.getElementById("container").appendChild(restart);
+        restart.onclick = function(){
+            myGameArea.start();
+            restart.remove();    
+        }
+    } 
+    
+        
+    myScore.text = "Score : " + point1; 
     myScore1.text = "Score : " + point2;
     myScore.update();
     myScore1.update();
